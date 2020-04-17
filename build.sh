d9fb41e3b22cad06eb07e193a58a3fba74b62366
Abdallah Arabyat <48789766+arabyat@users.noreply.github.com>
2020-04-17 19:58:01 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,10 @@
 
 
 echo "Abdallah Arabyat"
+
+sed -i "s/{HOSTNAME}/$HOSTNAME/g" web/index.html
+sed -i "s/{USER}/$USER/g" web/index.html
+sed -i "s/{GIT_BRANCH}/$(git rev-parse --abbrev-ref HEAD)/g" web/index.html
+sed -i "s/{GIT_REPO}/$(git config --get remote.origin.url)/g" web/index.html
+sed -i "s/{AZURE_VARIABLE}/$TEST/g" web/index.html
+sed -i "s/{PWD}/$dir/g" web/index.html
